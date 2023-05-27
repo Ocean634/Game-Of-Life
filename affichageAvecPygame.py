34b9925bc0564f73eca3c8b27fdab416c7f09970
Ocean6 <clenetjules@gmail.com>
2023-05-27 07:21:52 +0200

    Ajout de tous les fichiers pour  le premier commit

---
+++ affichageAvecPygame.py
@@ -0,0 +1,338 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Ocean6
+#
+# Created:     11/05/2023
+# Copyright:   (c) Ocean6 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# importation des modules innés
+import pygame
+import time
+import copy
+
+# importation du moteur du jeu de la vie que nous avons créé
+# utilisation d'un alias pour simplifier le code
+import projetJeuDeLaVie as jdlv
+
+import canon_planeur as cp
+
+def countdown(num_of_secs):
+    while num_of_secs:
+        m, s = divmod(num_of_secs, 60)
+        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+
+def compte_nb_cellules(world):
+    compteur=0
+    for ligne in world:
+        for case in ligne:
+            if case:
+                compteur+=1
+    return compteur
+
+def dessiner_grille(nb_colonnes, marge):
+    """Affiche le cadrillage du jeu sur l'écran
+
+    Parameters:
+        nb_colonnes (int): le nombre de colonnes de la matrice
+        marge (int): le nombre de pixel depuis le haut de la fenêtre où commencera le dessin
+
+    Returns:
+        None: None
+
+    """
+    # utilisation de variables globales car elles contiennent des données uniques "invariables"
+    global screen
+    global intervalle
+    global depart_gauche
+
+    # trouve la taille maximal du cadrillage qui rentre dans la fenêtre
+    intervalle_hauteur = (screen.get_height()-marge) // nb_colonnes
+    intervalle_largeur = (screen.get_width()-marge) // nb_colonnes
+    intervalle = intervalle_hauteur if intervalle_hauteur < intervalle_largeur else intervalle_largeur
+    # définie la coordonnée horizontale du bord gauche du cadrillage
+    depart_gauche = screen.get_width()/2 - intervalle*nb_colonnes/2
+
+    for ligne in range(nb_colonnes):
+        # dessine une ligne verticale et horizontale par colonne de la matrice
+        draw_line((intervalle*ligne + depart_gauche, marge/2), (intervalle*ligne + depart_gauche, nb_colonnes*intervalle + (marge/2)))
+        draw_line((depart_gauche, intervalle*ligne + (marge/2)), (intervalle*nb_colonnes + depart_gauche, ligne*intervalle + (marge/2)))
+    # dessine les dernières lignes verticale et horizontale du cadrillage
+    draw_line((intervalle*(ligne+1) + depart_gauche, marge/2), (intervalle*(ligne+1) + depart_gauche, nb_colonnes*intervalle + (marge/2)))
+    draw_line((depart_gauche, intervalle*(ligne+1) + (marge/2)), (intervalle*nb_colonnes + depart_gauche, (ligne+1)*intervalle + (marge/2)))
+
+
+def draw_line(pos1, pos2):
+    """Dessine une ligne sur l'écran
+
+    Parameters:
+        pos1 (tuple): couple des coordonnées de départ de la ligne
+        pos2 (tuple): couple des coordonnnées de fin de la ligne
+
+    Returns:
+        None: None
+
+    """
+    # utilisation d'une variable globale pour conserver un seul écran actif
+    global screen
+    pygame.draw.line(screen, 'black', pos1, pos2)
+
+
+def dessiner_cellules(world, marge):
+    """Dessine une ligne sur l'écran
+
+    Parameters:
+        pos1 (tuple): couple des coordonnées de départ de la ligne
+        pos2 (tuple): couple des coordonnnées de fin de la ligne
+
+    Returns:
+        None: None
+
+    """
+    global intervalle
+    global screen
+    global depart_gauche
+
+    # parcours toutes les cellules de la matrice
+    for i in range(len(world)):
+        for j in range(len(world[0])):
+            if world[i][j]:
+                # si la case contient une cellule vivante, la dessiner en noir
+                couleur = "black"
+            else:
+                # sinon la dessiner en blanc
+                couleur = "white"
+            pygame.draw.rect(screen, couleur, ((j*intervalle)+depart_gauche+1, (i*intervalle)+marge/2+1, intervalle-1, intervalle-1))
+
+
+def next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2,nb_cellules_init, precedente=False):
+    """Retourne la prochaine génération demandée par l'utilisateur
+
+    Parameters:
+        world (list): matrice du jeu de la vie
+        generation_affichee (int): numéro de la génération affichée actuellement sur l'écran
+        former_generations (list): matrice contenant les dix dernières générations générées
+        actual_gen (int): indice de la liste former_generations qui contient la matrice affichée actuellement à l'écran
+        derniere_gen (int): indice de la liste former_generations qui contient la derniere génération générée
+        precedente (bool): indique si l'utilisateur souhaite obtenir la précédente génération
+
+    Returns:
+        world (list): matrice du jeu de la vie
+        generation_affichee (int): numéro de la génération affichée actuellement sur l'écran
+        former_generations (list): matrice contenant les dix dernières générations générées
+        actual_gen (int): indice de la liste former_generations qui contient la matrice affichée actuellement à l'écran
+        derniere_gen (int): indice de la liste former_generations qui contient la derniere génération générée
+    """
+    font = pygame.font.SysFont(None, 30)
+    if precedente is False:
+        generation_affichee += 1
+
+        if actual_gen == derniere_gen:
+            # récupère la prochaine génération grâce au module jdlv
+            world = jdlv.next_generation(world)
+
+            # cas dans lequel la civilisation s'est stabilisé
+            if world in former_generations:
+                #instructions d'arrêt du programme
+                # créé une nouvelle matrice et réinitialise toutes les variables
+                world = jdlv.start(len(world))
+                if v2 is False:
+                    world = jdlv.init_world(world)
+                    nb_cellules_init=compte_nb_cellules(world)
+                else:
+                    world=jdlv.new_world(len(world), len(world))
+                former_generations = [[[False for i in range (len(world))] for j in range (len(world))] for i in range(10)]
+                actual_gen = 0
+                derniere_gen = 0
+                # copie profonde de la génération actuelle vers la mémoire
+                former_generations[actual_gen] = copy.deepcopy(world)
+                generation_affichee = 1
+            else:
+                # incrémentation des variables d'indice
+                actual_gen = 0 if actual_gen == 9 else actual_gen+1
+                derniere_gen = 0 if derniere_gen == 9 else derniere_gen+1
+                # copie profonde de la génération actuelle vers la mémoire
+                former_generations[actual_gen] = copy.deepcopy(world)
+
+        # cas où l'utilisateur serait revenu en arrière dans les générations
+        else:
+            actual_gen = 0 if actual_gen == 9 else actual_gen+1
+            world = former_generations[actual_gen]
+
+    elif precedente:
+        # tous les cas sauf lorsque la limite de la mémoire est atteinte
+        if actual_gen != derniere_gen+1 and derniere_gen != 9:
+            # décrémentation des variables
+            actual_gen = 9 if actual_gen==0 else actual_gen-1
+            generation_affichee -= 1
+            # actualisation de la génration actuelle
+            world = former_generations[actual_gen]
+    return world, generation_affichee, former_generations, actual_gen, derniere_gen
+
+def setup():
+    """Démarre pygame et créé l'écran
+
+    Parameters:
+        None: None
+
+    Returns:
+        None: None
+
+    """
+    global screen
+    global clock
+    pygame.init()
+    # créé une fenêtre de 1200 pixels de large et 600 pixels de haut
+    screen = pygame.display.set_mode((1200, 600))
+    pygame.display.set_caption("Programme du Jeu de la Vie")
+    clock = pygame.time.Clock()
+
+
+def run(nb_tick, marge, v2=False):
+    """Affiche chaque génération du jeu de la vie
+
+    Parameters:
+        nb_tick (int): nombre de tick entre chaque image affichée
+        marge (int): nombre de pixel de marge en haut de l'écran avant que la grille soit dessinée
+
+    Returns:
+        None: None
+
+    """
+    # cette fonction peut être considérée comme la fonction principale
+    global screen
+    global clock
+    global depart_gauche
+    global intervalle
+
+    # créé et initialise un nouvelle matrice
+    world = jdlv.start()
+    world = jdlv.init_world(world)
+    nb_cellules_init=compte_nb_cellules(world)
+    setup()
+    # nous commençons à la génration 1
+    generation_affichee = 1
+    # définit l'avancée des génrations sur pause
+    play = False
+    # initialise l'affichage du texte
+    font = pygame.font.SysFont(None, 30)
+    # cette liste est une mémoire qui contiendra les dix dernières générations
+    # cela permet de savoir si la civilisation s'est stabilisée
+    former_generations = [[[False for i in range (len(world))] for j in range (len(world))] for i in range(10)]
+    actual_gen = 0
+    derniere_gen = 0
+    former_generations[actual_gen] = copy.deepcopy(world)
+    # compteur qui permet d'afficher les générations à un rythme agréable
+    compteur_frames = 0
+
+    # créé les éléments visuels tels que les boutons etc...
+    fond = pygame.image.load("background.jpg").convert()
+    play_button = pygame.image.load("playButton.png").convert_alpha()
+    pause_button = pygame.image.load("pauseButton.png").convert_alpha()
+    next_button = pygame.image.load("nextButton.png").convert_alpha()
+    prev_button = pygame.image.load("prevButton.png").convert_alpha()
+    v2_button = pygame.image.load("button_v2.png").convert_alpha()
+    cannon_button = pygame.image.load("canon_button.png").convert_alpha()
+    rectangle_bouton_play = play_button.get_rect(topleft=(70, 50))
+    rectangle_bouton_pause = pause_button.get_rect(topleft=(160, 50))
+    rectangle_bouton_suivant = next_button.get_rect(topleft=(70, 500))
+    rectangle_bouton_precedent = prev_button.get_rect(topleft=(160, 500))
+    rectangle_button_v2= v2_button.get_rect(topleft=(70,150))
+    canon_vie_button=cannon_button.get_rect(topleft=(160,150))
+    gen_affichage=font.render(f"Génération actuelle : {str(generation_affichee)}",True,(0,0,0))
+    
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # permet de quitter la fenêtre si la croix est cliquée
+                running = False
+
+            if pygame.mouse.get_focused():
+                # récupère les coordonnées de la souris
+                x, y = pygame.mouse.get_pos()
+
+                # détecte les clics sur les boutons / cellules
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if rectangle_bouton_play.collidepoint(x, y):
+                        # si le bouton play est cliqué, l'avancée des générations démarre
+                        play = True
+                    if rectangle_bouton_pause.collidepoint(x, y):
+                        # si le bouton pause est cliqué, l'avancée des générations s'arrête
+                        play = False
+                    if rectangle_bouton_suivant.collidepoint(x, y):
+                        # déclenche la prochaine génération
+                        world, generation_affichee, former_generations, actual_gen, derniere_gen = \
+                        next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2, nb_cellules_init)
+                    if rectangle_bouton_precedent.collidepoint(x, y):
+                        # déclenche la précédente génération
+                        world, generation_affichee, former_generations, actual_gen, derniere_gen = \
+                        next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2,nb_cellules_init, precedente=True)
+                    if canon_vie_button.collidepoint(x,y):
+                        if len(world)>=36:
+                            world=jdlv.new_world(len(world),len(world))
+                            world=cp.canon_planeur(world)
+                        pygame.display.update()
+                    if play is False:
+                        if rectangle_button_v2.collidepoint(x,y):
+                            v2=not v2
+                            generation_affichee=1
+                            if v2:
+                                #on recrée une matrice cette fois vide
+                                world=jdlv.new_world(len(world), len(world))
+                            else:
+                                world=jdlv.start(len(world))
+                                world=jdlv.init_world(world)
+                                nb_cellules_init=compte_nb_cellules(world)
+            if pygame.mouse.get_pressed()[0]:
+                x,y=pygame.mouse.get_pos()
+                if play is False:
+                    # récupère les indices correspondants à la cellule cliquée
+                    coord_x = int((x - depart_gauche) / intervalle)
+                    coord_y = int((y - marge) / intervalle)
+                    # gère les clics dans le cdrillage
+                    if coord_x >= 0 and coord_y >= 0:
+                        try:
+                            world[coord_y][coord_x] = True
+                        except IndexError:
+                            None
+
+
+        # rafraichit l'écran
+        pygame.display.flip()
+
+        #actualise le compteur de génération
+        gen_affichage=font.render(f"Génération actuelle : {str(generation_affichee)}",True,(0,0,0))
+        # place les éléments visuels sur l'écran
+        screen.blit(fond, (0,0))
+        screen.blit(play_button, (70, 50))
+        screen.blit(pause_button, (160, 50))
+        screen.blit(next_button, (70, 500))
+        screen.blit(prev_button, (160, 500))
+        screen.blit(gen_affichage, (915, 50))
+        screen.blit(v2_button,(70,150))
+        screen.blit(cannon_button, (160,150))
+
+        dessiner_grille(len(world), marge*2)
+        dessiner_cellules(world, marge*2)
+
+        if play:
+            # permet d'afficher au rythme de deux images par seconde
+            if compteur_frames > nb_tick:
+                # déclenche la prochaine génération
+                world, generation_affichee, former_generations, actual_gen, derniere_gen = \
+                next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2, nb_cellules_init)
+                compteur_frames = 0
+
+        compteur_frames +=1
+        clock.tick(120)
+
+    pygame.quit()
+
+marge = 20
+nb_frame_sec = 10
+run(120/nb_frame_sec, marge)
---
+++ projetJeuDeLaVie.py
@@ -0,0 +1,229 @@
+# Créé par Ocean6, le 20/04/2023 en Python 3.11
+
+# importation des modules innés
+import copy
+import random
+import sys
+import time
+
+
+def new_world(nb_lignes, nb_colonnes):
+    """Retourne une matrice de taille donnée
+
+    Parameters:
+        nb_lignes (int): le nombre de lignes que la matrice contiendra
+        nb_colonnes (int): le nombre de colonnes que la matrice contiendra
+
+    Returns:
+        list: matrice générée
+
+    """
+    # création de la matrice par compréhention
+    world = [[False for i in range (nb_colonnes)] for j in range (nb_lignes)]
+    return world
+
+
+def init_world(world):
+    """Retourne une matrice remplie aléatoirement
+
+    Parameters:
+        world (list): la matrice à remplir
+
+    Returns:
+        list: matrice remplie
+
+   """
+    for i in range(len(world)):
+        for j in range(len(world[0])):
+            world[i][j]=bool(random.randint(0,1))
+    return world
+
+
+def afficher_world(world):
+    """Affiche dans la console les donées brutes de la matrice
+
+    Parameters:
+        world (list): la matrice à afficher
+
+    Returns:
+        None: None
+
+    """
+    for ligne in world:
+        for case in ligne:
+            print(case, end=" ")
+        print("")
+    return None
+
+
+def compte_voisins(world, x, y):
+    """Retourne le nombre de cellules vivantes autour d'une cellule choisie
+
+    Parameters:
+        world (list): la matrice contenant les cellules
+        x (int): coordonée x de la cellule choisie
+        y (int): coordonée y de la cellule choisie
+
+    Returns:
+        int: nombre de voisins trouvés autour de la cellule choisie
+
+   """
+    nb_voisins = 0
+    for i in range (y-1, y+2):
+        # évite d'obtenir l'indice -1 qui signifierait "la derniere ligne"
+        if i < 0:
+            continue
+
+        for j in range (x-1, x+2):
+            # évite d'obtenir l'indice -1 qui signifierait "la derniere colonne"
+            if j < 0:
+                continue
+            # évite de compter la cellule choisie comme voisine d'elle même
+            if j == x and i == y:
+                    continue
+            try:
+                if world[j][i] == True:
+                    nb_voisins += 1
+            # évite de faire planter le programme lorsque la cellule choisie est
+            # au bord du tableau, dans ce cas les cellules hors champ sont
+            # considérées comme mortes
+            except IndexError:
+                continue
+    return nb_voisins
+
+
+def next_generation (world):
+    """Retourne la génération suivante selon le jeu de la vie de John Conway
+
+    https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+    Parameters:
+        world (list): la matrice contenant les cellules
+
+    Returns:
+        list: matrice contenant les cellules de la génération suivante
+
+   """
+   # permet de réaliser une véritable copie complète de la matrice afin de ne
+   # pas écraser la matrice de la génération précédente
+    next_world = copy.deepcopy(world)
+
+    for i in range (len(world)):
+        for j in range (len(world[0])):
+            nb_voisins = compte_voisins(world, i, j)
+            # application des trois règles du jeu de la vie
+            if nb_voisins <= 1:
+                next_world[i][j] = False
+            if nb_voisins >= 4:
+                next_world[i][j] = False
+            if nb_voisins == 3:
+                next_world[i][j] = True
+    return next_world
+
+
+def bel_affichage (world):
+    """Affiche dans la console les donées de la matrice avec une mise en forme
+
+    Parameters:
+        world (list): la matrice à afficher
+
+    Returns:
+        None: None
+
+   """
+    print("|" + "-"*(len(world[0])*4-1) + "|")
+    for i in range (len(world)*2):
+        if i%2 == 1:
+            ligne = "-"*(len(world[0]*4)-1)
+        else:
+            ligne = ""
+            for j in range (len(world[0])):
+                if world[i//2][j] == True:
+                    chaine = "| * " if j != 0 else " * "
+                else:
+                    chaine = "|   " if j != 0 else "   "
+                ligne += chaine
+        print("|" + ligne + "|")
+    return None
+
+
+def start(largeur=None):
+    """Permet d'initialiser une matrice du jeu de la vie
+
+    Parameters:
+        largeur (int): la largeur de la matrice à créer
+
+    Returns:
+        list: matrice créée
+
+   """
+    if largeur == None:
+        largeur = try_input("Saisissez la largeur de la grille:")
+
+        # gestion des entrées de l'utilisateur
+        while True:
+            if largeur == "":
+                largeur = try_input("La valeur que vous venez d'entrer est vide, veuillez saisir\nun nombre entier positif compris entre 1 et 100 : ")
+                continue
+            try:
+                largeur = int(largeur)
+            except ValueError:
+                largeur = try_input("La valeur que vous venez d'entrer (\"" + largeur + "\")\nn'est pas un nombre. Veuillez rééssayer : ")
+                continue
+            if largeur <= 0:
+                largeur = try_input("Le nombre que vous avez entré n'est pas strictement positif,\nveuillez entrer un nombre entre 1 et 100 : ")
+                continue
+            if largeur > 100:
+                largeur = try_input("Le nombre que vous avez entré est trop grand pour que l'affichage puisse se faire correctement.\nVeuillez entrer un nombre entre 1 et 100 :")
+                continue
+            break
+
+    world = new_world(largeur, largeur)
+    return world
+
+
+def try_input(message):
+    """Permet à l'utilisateur d'entrer une valeur au clavier proprement
+
+    Parameters:
+        message (str): message qui va être afficher dans la boite de dialogue
+
+    Returns:
+        str: valeur entrée par l'utilisateur
+
+   """
+    try:
+        inputTest = input(message)
+    # permet d'éviter de faire planter le programme si la boite de dialogue est fermée
+    except KeyboardInterrupt:
+        print("Programme arrêté après la fermeture d'une boîte de saisie.")
+        sys.exit()
+    return inputTest
+
+
+def main():
+    world = start()
+    bel_affichage(world)
+    nb_generation = try_input("Saisissez le nombre de génération que vous souhaitez générer :")
+
+    # gestion des entrées de l'utilisateur
+    while True:
+            if nb_generation == "":
+                largeur = try_input("La valeur que vous venez d'entrer est vide, veuillez saisir\nun nombre entier positif supérieur à 1 : ")
+                continue
+            try:
+                nb_generation = int(nb_generation)
+            except ValueError:
+                nb_generation = try_input("La valeur que vous venez d'entrer (\"" + nb_generation + "\")\nn'est pas un nombre. Veuillez rééssayer : ")
+                continue
+            if nb_generation <= 0:
+                nb_generation = try_input("Le nombre que vous avez entré n'est pas strictement positif,\nveuillez entrer un nombre supérieur à 1 : ")
+                continue
+            break
+
+    for i in range(nb_generation):
+        world = next_generation(world)
+        print("")
+        bel_affichage(world)
+        time.sleep(0.5)
+#main()
