0cd0e8c6dcc2d68c354b9cf67d48592517a14fb1
Leandro <lelenob34@gmail.com>
2023-05-29 18:32:26 +0200

    fenêtre d'acceuil

--- affichageAvecPygame.py
+++ affichageAvecPygame.py
@@ -10,7 +10,7 @@
 #-------------------------------------------------------------------------------
 
 # importation des modules innés
-import pygame
+import pygame, sys
 import time
 import copy
 
@@ -20,10 +20,125 @@ import projetJeuDeLaVie as jdlv
 
 import canon_planeur as cp
 
-def countdown(num_of_secs):
-    while num_of_secs:
-        m, s = divmod(num_of_secs, 60)
-        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+mainClock = pygame.time.Clock()
+
+def blit_text(surface, text, pos, font, color=pygame.Color('black')):
+    words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+    space = font.size(' ')[0]  # The width of a space.
+    max_width, max_height = surface.get_size()
+    x, y = pos
+    for line in words:
+        for word in line:
+            word_surface = font.render(word, 0, color)
+            word_width, word_height = word_surface.get_size()
+            if x + word_width >= max_width:
+                x = pos[0]  # Reset the x.
+                y += word_height  # Start on new row.
+            surface.blit(word_surface, (x, y))
+            x += word_width + space
+        x = pos[0]  # Reset the x.
+        y += word_height  # Start on new row.
+
+def welcome():
+    fullscreen=False
+    font = pygame.font.SysFont(None, 60)
+    font1 = pygame.font.SysFont(None, 30)
+    running = True
+    fond = pygame.image.load("background.jpg").convert()
+    play_button=pygame.image.load("pygame_run.png").convert_alpha()
+    welcome_title=font.render("Le Jeu de la vie", True, (0,0,0))
+    play_game_txt=font1.render("Voir la simulation :",True,(0,0,0))
+    welcome_txt_p1="Bienvenue dans la fascinante simulation du Jeu de la Vie ! Dans cet univers virtuel, vous avez le pouvoir de créer et de contrôler la vie elle-même. Vous êtes sur le point d'embarquer dans un voyage captivant où vous serez témoin de l'évolution, de la complexité et de la beauté de la vie, tout en explorant les principes fondamentaux qui la gouvernent."
+    welcome_txt_p2="Dans cette simulation, vous vous trouverez face à une grille constituée de cellules. Chaque cellule peut être dans l'un des deux états possibles : vivante ou morte. En interagissant avec ces cellules, vous découvrirez comment elles évoluent au fil du temps en fonction de règles simples mais fascinantes."
+    welcome_txt_p3="Votre rôle en tant qu'observateur et joueur est crucial. Vous pouvez créer de nouvelles cellules, les faire interagir et les voir se multiplier, se déplacer et même s'éteindre. La façon dont vous manipulez ces cellules et les règles que vous établissez auront un impact direct sur l'évolution de cet écosystème numérique."
+    welcome_txt_p4= "Mais ne vous méprenez pas, cette simulation est plus qu'un simple jeu. Elle porte en elle des principes complexes de dynamique, d'émergence et de systèmes complexes. Vous pourrez observer l'apparition de schémas étonnants, de structures stables et même de formes de vie émergentes à partir de règles de base."
+    welcome_txt_p5="Préparez-vous à être émerveillé par la puissance de cette simulation et à vous émerger dans un monde où la vie prend vie grâce à votre imagination et à vos décisions. Prêt à plonger dans l'univers du Jeu de la Vie ? Alors, préparez-vous à explorer, à expérimenter et à apprendre en jouant avec les fondements mêmes de la vie !"
+    play_button_vie=play_button.get_rect(topleft=(260,35))
+    screen.blit(fond, (0,0))
+    while running:
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+            if pygame.mouse.get_pressed()[0]:
+                x,y=pygame.mouse.get_pos()
+                if play_button_vie.collidepoint(x,y):
+                    run(120/nb_frame_sec, marge, world)
+        screen.blit(welcome_title, (450,20))
+        screen.blit(play_game_txt, (70,50))
+        screen.blit(play_button, (260,35))
+        blit_text(screen, welcome_txt_p1, (70,100), font1)
+        blit_text(screen, welcome_txt_p2, (70,200), font1)
+        blit_text(screen, welcome_txt_p3, (70,300), font1)
+        blit_text(screen, welcome_txt_p4, (70,400), font1)
+        blit_text(screen, welcome_txt_p5, (70,500), font1)
+        
+        pygame.display.update()
+        mainClock.tick(60)
+
+def options():
+    global world
+    font = pygame.font.SysFont(None, 60)
+    font1 = pygame.font.SysFont(None, 30)
+    running = True
+    fond = pygame.image.load("background.jpg").convert()
+    cannon_button = pygame.image.load("canon_button.png").convert_alpha()
+    moulin_button = pygame.image.load("moulin_img.png").convert_alpha()
+    retour_button = pygame.image.load("retour_img.png").convert_alpha()
+    canon_vie_button=cannon_button.get_rect(topleft=(70,350))
+    moulin_vie_button=moulin_button.get_rect(topleft=(70,250))
+    retour_vie_button=retour_button.get_rect(topleft=(70,60))
+    menu_title=font.render("Fonctionalités",True,(0,0,0))
+    canon_txt=font1.render("crée une figure qui lance des planeurs, cependant elle nécessite une grille avec une largeur supérieur 39",True,(0,0,0))
+    moulin_txt=font1.render("crée une figure clignotante plutôt curieuse, cependant elle nécessite une grille avec une largeur supérieur 9",True,(0,0,0))
+    screen.blit(fond, (0,0))
+    while running:
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+            if pygame.mouse.get_pressed()[0]:
+                # récupère les coordonnées de la souris
+                x, y = pygame.mouse.get_pos()
+                if retour_vie_button.collidepoint(x,y):
+                    running=False
+                if canon_vie_button.collidepoint(x,y):
+                    if len(world)>=36:
+                        world=jdlv.new_world(len(world),len(world))
+                        world=cp.canon_planeur(world)
+                        run(120/nb_frame_sec, marge, world)
+                if moulin_vie_button.collidepoint(x,y):
+                    if len(world)>=9:
+                        world=jdlv.new_world(len(world),len(world))
+                        world=cp.moulin_figure(world)
+                        run(120/nb_frame_sec, marge, world)
+        x,y=pygame.mouse.get_pos()
+        if moulin_vie_button.collidepoint(x,y):
+            screen.blit(moulin_txt, (130,265))
+        else:
+            screen.blit(fond, (0,0))
+        x,y=pygame.mouse.get_pos()
+        if canon_vie_button.collidepoint(x,y):
+            screen.blit(canon_txt, (130,365))
+        else:
+            screen.blit(fond, (0,0))
+
+        screen.blit(cannon_button, (70,350))
+        screen.blit(moulin_button, (70,250))
+        screen.blit(menu_title, (450,60))
+        screen.blit(retour_button, (70,60))
+        pygame.display.update()
+        mainClock.tick(60)
+
+
 
 def compte_nb_cellules(world):
     compteur=0
@@ -108,7 +223,7 @@ def dessiner_cellules(world, marge):
             pygame.draw.rect(screen, couleur, ((j*intervalle)+depart_gauche+1, (i*intervalle)+marge/2+1, intervalle-1, intervalle-1))
 
 
-def next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2,nb_cellules_init, precedente=False):
+def next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2, precedente=False):
     """Retourne la prochaine génération demandée par l'utilisateur
 
     Parameters:
@@ -141,7 +256,6 @@ def next_generation(world, generation_affichee, former_generations, actual_gen, 
                 world = jdlv.start(len(world))
                 if v2 is False:
                     world = jdlv.init_world(world)
-                    nb_cellules_init=compte_nb_cellules(world)
                 else:
                     world=jdlv.new_world(len(world), len(world))
                 former_generations = [[[False for i in range (len(world))] for j in range (len(world))] for i in range(10)]
@@ -171,7 +285,7 @@ def next_generation(world, generation_affichee, former_generations, actual_gen, 
             # actualisation de la génration actuelle
             world = former_generations[actual_gen]
     return world, generation_affichee, former_generations, actual_gen, derniere_gen
-
+monitor_size=[]
 def setup():
     """Démarre pygame et créé l'écran
 
@@ -186,12 +300,13 @@ def setup():
     global clock
     pygame.init()
     # créé une fenêtre de 1200 pixels de large et 600 pixels de haut
-    screen = pygame.display.set_mode((1200, 600))
+    monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
+    screen = pygame.display.set_mode((1200, 600), pygame.RESIZABLE)
     pygame.display.set_caption("Programme du Jeu de la Vie")
     clock = pygame.time.Clock()
 
 
-def run(nb_tick, marge, v2=False):
+def run(nb_tick, marge, world, v2=False):
     """Affiche chaque génération du jeu de la vie
 
     Parameters:
@@ -209,10 +324,9 @@ def run(nb_tick, marge, v2=False):
     global intervalle
 
     # créé et initialise un nouvelle matrice
-    world = jdlv.start()
+    """world = jdlv.start()
     world = jdlv.init_world(world)
-    nb_cellules_init=compte_nb_cellules(world)
-    setup()
+    setup()"""
     # nous commençons à la génration 1
     generation_affichee = 1
     # définit l'avancée des génrations sur pause
@@ -234,14 +348,14 @@ def run(nb_tick, marge, v2=False):
     pause_button = pygame.image.load("pauseButton.png").convert_alpha()
     next_button = pygame.image.load("nextButton.png").convert_alpha()
     prev_button = pygame.image.load("prevButton.png").convert_alpha()
+    menu_button = pygame.image.load("dropdown_menu.png").convert_alpha()
     v2_button = pygame.image.load("button_v2.png").convert_alpha()
-    cannon_button = pygame.image.load("canon_button.png").convert_alpha()
     rectangle_bouton_play = play_button.get_rect(topleft=(70, 50))
     rectangle_bouton_pause = pause_button.get_rect(topleft=(160, 50))
     rectangle_bouton_suivant = next_button.get_rect(topleft=(70, 500))
     rectangle_bouton_precedent = prev_button.get_rect(topleft=(160, 500))
-    rectangle_button_v2= v2_button.get_rect(topleft=(70,150))
-    canon_vie_button=cannon_button.get_rect(topleft=(160,150))
+    rectangle_button_v2= v2_button.get_rect(topleft=(900,20))
+    menu_vie_button=menu_button.get_rect(topleft=(1135,20))
     gen_affichage=font.render(f"Génération actuelle : {str(generation_affichee)}",True,(0,0,0))
     
 
@@ -267,16 +381,13 @@ def run(nb_tick, marge, v2=False):
                     if rectangle_bouton_suivant.collidepoint(x, y):
                         # déclenche la prochaine génération
                         world, generation_affichee, former_generations, actual_gen, derniere_gen = \
-                        next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2, nb_cellules_init)
+                        next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2)
                     if rectangle_bouton_precedent.collidepoint(x, y):
                         # déclenche la précédente génération
                         world, generation_affichee, former_generations, actual_gen, derniere_gen = \
-                        next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2,nb_cellules_init, precedente=True)
-                    if canon_vie_button.collidepoint(x,y):
-                        if len(world)>=36:
-                            world=jdlv.new_world(len(world),len(world))
-                            world=cp.canon_planeur(world)
-                        pygame.display.update()
+                        next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2, precedente=True)
+                    if menu_vie_button.collidepoint(x,y):
+                        options()
                     if play is False:
                         if rectangle_button_v2.collidepoint(x,y):
                             v2=not v2
@@ -287,9 +398,9 @@ def run(nb_tick, marge, v2=False):
                             else:
                                 world=jdlv.start(len(world))
                                 world=jdlv.init_world(world)
-                                nb_cellules_init=compte_nb_cellules(world)
             if pygame.mouse.get_pressed()[0]:
                 x,y=pygame.mouse.get_pos()
+
                 if play is False:
                     # récupère les indices correspondants à la cellule cliquée
                     coord_x = int((x - depart_gauche) / intervalle)
@@ -313,9 +424,9 @@ def run(nb_tick, marge, v2=False):
         screen.blit(pause_button, (160, 50))
         screen.blit(next_button, (70, 500))
         screen.blit(prev_button, (160, 500))
-        screen.blit(gen_affichage, (915, 50))
-        screen.blit(v2_button,(70,150))
-        screen.blit(cannon_button, (160,150))
+        screen.blit(gen_affichage, (915, 550))
+        screen.blit(menu_button, (1135, 20))
+        screen.blit(v2_button,(900,20))
 
         dessiner_grille(len(world), marge*2)
         dessiner_cellules(world, marge*2)
@@ -325,14 +436,16 @@ def run(nb_tick, marge, v2=False):
             if compteur_frames > nb_tick:
                 # déclenche la prochaine génération
                 world, generation_affichee, former_generations, actual_gen, derniere_gen = \
-                next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2, nb_cellules_init)
+                next_generation(world, generation_affichee, former_generations, actual_gen, derniere_gen,v2)
                 compteur_frames = 0
 
         compteur_frames +=1
         clock.tick(120)
 
     pygame.quit()
-
+world = jdlv.start()
+world = jdlv.init_world(world)
+setup()
 marge = 20
 nb_frame_sec = 10
-run(120/nb_frame_sec, marge)
+welcome()
